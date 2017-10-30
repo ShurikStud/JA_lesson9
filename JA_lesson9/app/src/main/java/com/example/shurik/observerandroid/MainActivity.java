f304f7a491574be7cb828f230301886c3ab62e54
USER-PC\shurik <abashev.alexandr@gmail.com>
2017-10-30 20:50:40 +0200

    рабочая версия

--- JA_lesson9/app/src/main/java/com/example/shurik/observerandroid/MainActivity.java
+++ JA_lesson9/app/src/main/java/com/example/shurik/observerandroid/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.shurik.observerandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "===========1============");
+
+        final Data data   = new Data();
+        final DisplayData displayData = new DisplayData(data);
+
+        Log.v(TAG, "===========2============");
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //displayData.update("new string");
+                        data.setData("new string");
+                        Toast.makeText(MainActivity.this, "Toast", Toast.LENGTH_LONG).show();
+                        displayData.removeObserverFromData();
+                    }
+                });
+            }
+        });
+
+        try {
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        thread.start();
+
+        Log.v(TAG, "===========3============");
+
     }
 }
